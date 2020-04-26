a5b1b9f437f43d5d15b74fda27625bf18aecad60
penadidik <di2k.skh@gmail.com>
2020-04-26 19:02:40 +0700

    model table tac

---
+++ src/main/kotlin/org/lintaspena/elearning/model/TAC.kt
@@ -0,0 +1,19 @@
+package org.lintaspena.elearning.model
+
+import org.lintaspena.elearning.base.BaseModel
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Table
+import javax.persistence.UniqueConstraint
+
+@Entity(name = "tb_m_tac")
+@Table(uniqueConstraints = [UniqueConstraint(columnNames = ["judul_tac"])])
+class TAC: BaseModel() {
+
+    @Column(unique = true, name = "judul_tac", length = 50)
+    var judulTAC: String? = ""
+
+    @Column(name = "deskripsi_tac")
+    var deskripsiTAC: String? = ""
+
+}
