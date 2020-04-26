665d22b5a62992072b5438d9643b29491886718b
penadidik <di2k.skh@gmail.com>
2020-04-26 19:02:32 +0700

    model table slide

---
+++ src/main/kotlin/org/lintaspena/elearning/model/Slide.kt
@@ -0,0 +1,22 @@
+package org.lintaspena.elearning.model
+
+import org.lintaspena.elearning.base.BaseModel
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Table
+import javax.persistence.UniqueConstraint
+
+@Entity(name = "tb_m_slide")
+@Table(uniqueConstraints = [UniqueConstraint(columnNames = ["judul_slide"])])
+class Slide: BaseModel() {
+
+    @Column(unique = true, name = "judul_slide", length = 50)
+    var judulSlide: String? = ""
+
+    @Column(name = "deskripsi_slide")
+    var deskripsiSlide: String? = ""
+
+    @Column(name = "gambar_slide")
+    var gambarSlide: String? = ""
+
+}
