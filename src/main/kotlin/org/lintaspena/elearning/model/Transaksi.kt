d93790784027a71d19bddd38abcff8f495ee5e70
penadidik <di2k.skh@gmail.com>
2020-04-26 21:23:14 +0700

    model table transaksi

---
+++ src/main/kotlin/org/lintaspena/elearning/model/Transaksi.kt
@@ -0,0 +1,42 @@
+package org.lintaspena.elearning.model
+
+import org.lintaspena.elearning.base.BaseModel
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+
+@Entity(name = "tb_m_transaksi")
+class Transaksi: BaseModel() {
+
+    @Column(name = "validasi_learning")
+    var validasiLearning: Int? = 0
+
+    @Column(name = "nilai_akhir")
+    var nilaiAkhir: Int? = 0
+
+    @Column(name = "keterangan")
+    var keterangan: String? = ""
+
+    @Column(name = "sertifikat")
+    var sertifikat: String? = ""
+
+    @Column(name = "validasi_pembayaran")
+    var validasiPembayaran: String? = ""
+
+    @Column(name = "upload_pembayaran")
+    var uploadPembayaran: String? = ""
+
+    @ManyToOne
+    @JoinColumn(name = "id_peserta", nullable = false)
+    var peserta: User? = null
+
+    @ManyToOne
+    @JoinColumn(name = "id_kelas", nullable = false)
+    var kelas: Kelas? = null
+
+    @ManyToOne
+    @JoinColumn(name = "id_metode", nullable = false)
+    var metodePembayaran: MetodePembayaran? = null
+
+}
