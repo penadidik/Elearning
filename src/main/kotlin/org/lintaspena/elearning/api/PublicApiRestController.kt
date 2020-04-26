78df3b6fa5ea3f0528f107ad67f34b5315496488
penadidik <di2k.skh@gmail.com>
2020-04-26 22:23:45 +0700

    update rest api bantuan

---
+++ src/main/kotlin/org/lintaspena/elearning/api/PublicApiRestController.kt
@@ -0,0 +1,32 @@
+package org.lintaspena.elearning.api
+
+import org.apache.logging.log4j.LogManager
+import org.lintaspena.elearning.moduls.bantuan.Bantuan
+import org.lintaspena.elearning.moduls.bantuan.service.BantuanService
+import org.lintaspena.elearning.utils.base.BaseController
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/public")
+class PublicApiRestController: BaseController() {
+
+    val logger = LogManager.getLogger(PublicApiRestController::class.java)
+
+
+    // -- Rest API Bantuan
+    @Autowired
+    private lateinit var bantuanService: BantuanService
+
+    @GetMapping("/bantuan/all")
+    fun getViewAllBantuan(): List<Bantuan> = bantuanService.viewAllBantuan()
+
+    @GetMapping("/bantuan/findById")
+    fun getBantuanById(@RequestParam("idBantuan") id : Long): Bantuan {
+        return bantuanService.findBantuanById(id)
+    }
+
+}
