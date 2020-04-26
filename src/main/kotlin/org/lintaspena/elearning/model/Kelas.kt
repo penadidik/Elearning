4a01b6c7f364c4340f93b69482e5d5948d0dccef
penadidik <di2k.skh@gmail.com>
2020-04-26 20:44:58 +0700

    model table kelas

---
+++ src/main/kotlin/org/lintaspena/elearning/model/Kelas.kt
@@ -0,0 +1,26 @@
+package org.lintaspena.elearning.model
+
+import org.lintaspena.elearning.base.BaseModel
+import javax.persistence.*
+
+@Entity(name = "tb_m_kelas")
+@Table(uniqueConstraints = [UniqueConstraint(columnNames = ["nama_kelas"])])
+class Kelas: BaseModel() {
+
+    @Column(unique = true, name = "nama_kelas", length = 50)
+    var namaKelas: String? = ""
+
+    @Column(name = "deskripsi_kelas")
+    var deskripsiKelas: String? = ""
+
+    @Column(name = "jumlah_materi")
+    var jumlahMater: Int? = 0
+
+    @Column(name = "harga_kelas")
+    var hargaKelas: Int? = 0
+
+    @ManyToOne
+    @JoinColumn(name = "id_kategori", nullable = false)
+    var kategoriKursus: KategoriKursus? = null
+
+}
