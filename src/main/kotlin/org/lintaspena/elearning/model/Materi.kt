fd2f473b2851d1be21afd408dd930a07ff596e2f
penadidik <di2k.skh@gmail.com>
2020-04-26 20:44:50 +0700

    model table materi

---
+++ src/main/kotlin/org/lintaspena/elearning/model/Materi.kt
@@ -0,0 +1,27 @@
+package org.lintaspena.elearning.model
+
+import org.lintaspena.elearning.base.BaseModel
+import javax.persistence.*
+
+@Entity(name = "tb_m_materi")
+@Table(uniqueConstraints = [UniqueConstraint(columnNames = ["nama_materi"])])
+class Materi: BaseModel() {
+
+    @Column(unique = true, name = "nama_materi", length = 50)
+    var namaMateri: String? = ""
+
+    @Column(name = "deskripsi_materi")
+    var deskripsiMateri: String? = ""
+
+    @Column(name = "is_materi")
+    var isiMateri: String? = ""
+
+    @ManyToOne
+    @JoinColumn(name = "id_pemateri", nullable = false)
+    var pemateri: User? = null
+
+    @ManyToOne
+    @JoinColumn(name = "id_kelas", nullable = false)
+    var kelas: Kelas? = null
+
+}
