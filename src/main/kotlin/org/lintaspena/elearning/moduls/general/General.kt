51b3df5a935c0550a705b8d9b5cd3dbe1acae439
penadidik <di2k.skh@gmail.com>
2020-04-26 21:49:40 +0700

    modul general

--- src/main/kotlin/org/lintaspena/elearning/moduls/general/General.kt
+++ src/main/kotlin/org/lintaspena/elearning/moduls/general/General.kt
@@ -1,6 +1,6 @@
-package org.lintaspena.elearning.model
+package org.lintaspena.elearning.moduls.general
 
-import org.lintaspena.elearning.base.BaseModel
+import org.lintaspena.elearning.utils.base.BaseModel
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.Table
---
+++ src/main/kotlin/org/lintaspena/elearning/moduls/general/GeneralRepo.kt
@@ -0,0 +1,5 @@
+package org.lintaspena.elearning.moduls.general
+
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface GeneralRepo : JpaRepository<General, Long>
